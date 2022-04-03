712444e3e019d6ef39f0eda0a8a93fb7d31aa40d
Ian Pilipski <ian@pilipski.com>
2022-04-03 19:37:15 -0400

    moved audio into a package

--- IPTech-UnityLibrary/Packages/IPTech.Module/Runtime/Constants.cs
+++ IPTech-UnityLibrary/Packages/IPTech.Module/Runtime/Constants.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-namespace IPTech.Module {
+﻿namespace IPTech.Module {
     public static class Constants {
         public const string MenuName = "IPTech";
     }
