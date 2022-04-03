03db89c0b8caaa8a1347225e51c48b96a0b9326c
Ian Pilipski <ian@pilipski.com>
2022-04-03 19:37:15 -0400

    moved audio into a package

---
+++ IPTech-UnityLibrary/Packages/com.iptech.audio/Runtime/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("IPTech.Audio.Editor")]
+[assembly: InternalsVisibleTo("IPTech.Audio.Runtime.Tests")]
---
+++ IPTech-UnityLibrary/Packages/com.iptech.editorextensions/Runtime/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("IPTech.EditorExtensions.Editor")]
+[assembly: InternalsVisibleTo("IPTech.EditorExtensions.Runtime.Tests")]
