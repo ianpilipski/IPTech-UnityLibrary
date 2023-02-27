ea3ec0406932bb95340e7012a333900ac43fc8f5
Ian Pilipski <ian@pilipski.com>
2023-02-27 01:10:48 -0500

    feat: add the ability to modify the buildplayer options from configmodifiers

--- IPTech-UnityLibrary/Packages/com.iptech.buildtool/Editor/Source/ConfigModifiers/ConfigModifier.cs
+++ IPTech-UnityLibrary/Packages/com.iptech.buildtool/Editor/Source/ConfigModifiers/ConfigModifier.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 namespace IPTech.BuildTool {
     public abstract class ConfigModifier : ScriptableObject {
         public abstract void ModifyProject();
         public abstract void RestoreProject();
+        public virtual BuildPlayerOptions ModifyBuildPlayerOptions(BuildPlayerOptions options) { return options; }
     }
 }
