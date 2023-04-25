01b26f24408fad8a7e1bac696c2ecac257ce957f
Ian Pilipski <ian@pilipski.com>
2023-04-25 00:10:33 -0400

    refactor: added decrypt item to config processors

--- IPTech-UnityLibrary/Packages/com.iptech.buildtool/Editor/Source/Processors/BuildProcessor.cs
+++ IPTech-UnityLibrary/Packages/com.iptech.buildtool/Editor/Source/Processors/BuildProcessor.cs
@@ -1,3 +1,7 @@
+using System;
+using IPTech.BuildTool.Editors;
+using IPTech.BuildTool.Encryption;
+using UnityEditor.Build;
 using UnityEditor.Build.Reporting;
 using UnityEngine;
 
@@ -7,5 +11,13 @@ namespace IPTech.BuildTool
         public virtual void PostProcessBuild(BuildReport report) { }
         public virtual void PreprocessBuild(BuildReport report) { }
         public virtual void PostGenerateGradleAndroidProject(string path) { }
+
+        protected T GetDecryptedItem<T>(EncryptedItem<T>.Reference item) where T : EncryptedItem<T> {
+            try {
+                return BuildToolsSettings.instance.GetDecryptedItem(item);
+            } catch(Exception e) {
+                throw new BuildFailedException(e);
+            }
+        }
     }
 }
