6734bcb3c47f0878d79284b0023cd46335bec376
Ian Pilipski <ian@pilipski.com>
2023-08-21 12:26:54 -0400

    chore: updated some files

--- IPTech-UnityLibrary/Packages/com.iptech.clickerengine/Runtime/Source/UnityLayer/ClickerGame.cs
+++ IPTech-UnityLibrary/Packages/com.iptech.clickerengine/Runtime/Source/UnityLayer/ClickerGame.cs
@@ -16,25 +16,7 @@ namespace IPTech.ClickerLibrary {
 		
 		private bool debugdraw = false;
 		
-		/*
-		object ShowDebug(params string[] args) {
-			debugdraw = !debugdraw;
-			return "ShowDebug is " + (debugdraw ? "On" : "Off"); 
-		}
 		
-		object Click(params string[] args) {
-			int amount = DebugConsole.ArgAsType<int>(args,1,1);
-			clickerengine.Click (amount);
-			return "Clicked " + amount.ToString ();
-		}
-		
-		object Purchase(params string[] args) {
-			int index = DebugConsole.ArgAsType<int>(args,1,0);
-			ClickerEngine.PurchaseClickerRetVal retVal = clickerengine.PurchaseClicker(index);
-			return retVal;
-		}
-		*/
-
 		// Use this for initialization
 		virtual public void Awake () {
 			clickerengine = new ClickerEngine();
@@ -45,12 +27,11 @@ namespace IPTech.ClickerLibrary {
 				}
 			}
 		}
-		
-		virtual public void Start() {
-			//DebugConsole.RegisterCommand("ShowDebug",ShowDebug);
-			//DebugConsole.RegisterCommand("Purchase",Purchase);
-			//DebugConsole.RegisterCommand("Click",Click);
+
+		virtual public void Click() {
+			clickerengine.Click();
 		}
+		
 		
 		private IEnumerator AutoTickCoroutine() {
 			while (true) {
@@ -62,9 +43,9 @@ namespace IPTech.ClickerLibrary {
 		private Vector2 scrollPos;
 		void OnGUI() {
 			if(Application.platform == RuntimePlatform.IPhonePlayer) {
-				if(GUILayout.Button ("toggle console")) {
+				//if(GUILayout.Button ("toggle console")) {
 					//DebugConsole.IsOpen = !DebugConsole.IsOpen;
-				}
+				//}
 			} else if(Event.current.isKey && Event.current.keyCode == KeyCode.BackQuote) {
 				//DebugConsole.IsOpen = !DebugConsole.IsOpen;
 			}
