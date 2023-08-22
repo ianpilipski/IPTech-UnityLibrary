3dacbe05eaefdce110dd34a88800c00592966cfa
Ian Pilipski <ian@pilipski.com>
2023-08-22 01:42:35 -0400

    bugfix(slotengine): fixed serialization after moving classes to new assembly

--- IPTech-UnityLibrary/Packages/com.iptech.slotengine/Runtime/Source/SerializationUtil.cs
+++ IPTech-UnityLibrary/Packages/com.iptech.slotengine/Runtime/Source/SerializationUtil.cs
@@ -2,7 +2,10 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Reflection;
+using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
+using IPTech.SlotEngine.Model.Api;
 using UnityEngine;
 
 namespace IPTech.SlotEngine {
@@ -20,9 +23,44 @@ namespace IPTech.SlotEngine {
         public static object DeserializeFromBase64String(string base64String) {
             var serializer = new BinaryFormatter();
 
+            serializer.Binder = new CustomSerializationBinder();
+            
             byte[] bytes = Convert.FromBase64String(base64String);
 			using(var stream = new MemoryStream(bytes)) {
 			    return serializer.Deserialize(stream);
+            }
+        }
+
+        class CustomSerializationBinder : SerializationBinder
+        {
+            static Dictionary<string, Type> knownTypes = new Dictionary<string, Type>() {
+                { "System.Collections.Generic.List`1[[IPTech.SlotEngine.Model.Api.IPayline, Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null]]",
+                    typeof(List<IPayline>) },
+                { "System.Collections.Generic.List`1[[IPTech.SlotEngine.Model.Api.IPayoutTableEntry, Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null]]",
+                    typeof(List<IPayoutTableEntry>) },
+                { "System.Collections.Generic.List`1[[IPTech.SlotEngine.Model.Api.IReel, Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null]]",
+                    typeof(List<IReel>)},
+                { "System.Collections.Generic.List`1[[IPTech.SlotEngine.Model.Api.IWildSymbol, Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null]]",
+                    typeof(List<IWildSymbol>)},
+                { "System.Collections.Generic.List`1[[IPTech.SlotEngine.Model.Api.IPaylineEntry, Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null]]",
+                    typeof(List<IPaylineEntry>)},
+                { "System.Collections.Generic.List`1[[IPTech.SlotEngine.Model.Api.ISymbol, Assembly-CSharp, Version=0.0.0.0, Culture=neutral, PublicKeyToken=null]]",
+                    typeof(List<ISymbol>)}
+            };
+
+            public override Type BindToType(string assemblyName, string typeName)
+            {
+                //Debug.Log($"BindToType: {assemblyName} : {typeName}");
+                
+                if(assemblyName.Equals("Assembly-CSharp")) {
+                    return Type.GetType(typeName);
+                }
+
+                if(knownTypes.TryGetValue(typeName, out Type t)) {
+                    return t;
+                }
+
+                return null;
             }
         }
     }
