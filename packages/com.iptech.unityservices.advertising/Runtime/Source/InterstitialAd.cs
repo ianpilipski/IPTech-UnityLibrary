f07fd36009b7eec54a05b36f13f24c28839e1e2f
Ian Pilipski <ian@pilipski.com>
2023-10-23 22:37:19 -0400

    feat(unityservices): added an event on the AdsManager to fire when an ad is shown

--- packages/com.iptech.unityservices.advertising/Runtime/Source/InterstitialAd.cs
+++ packages/com.iptech.unityservices.advertising/Runtime/Source/InterstitialAd.cs
@@ -31,25 +31,24 @@ namespace IPTech.UnityServices.Internal {
         #region AdInfo Interstitial
         // Invoked when the interstitial ad was loaded succesfully.
         override protected void InterstitialOnAdReadyEvent(IronSourceAdInfo adInfo) {
-            Debug.Log("unity-script: I got InterstitialOnAdReadyEvent With AdInfo " + adInfo.ToString());
+            result.Info = adInfo;
             loadState = LoadState.Loaded;
         }
 
         // Invoked when the initialization process has failed.
         override protected void InterstitialOnAdLoadFailed(IronSourceError ironSourceError) {
-            Debug.Log("unity-script: I got InterstitialOnAdLoadFailed With Error " + ironSourceError.ToString());
             loadState = LoadState.FailedToLoad;
             result.AdResult = AdResult.FaildToLoad;
         }
 
         // Invoked when the Interstitial Ad Unit has opened. This is the impression indication. 
         override protected void InterstitialOnAdOpenedEvent(IronSourceAdInfo adInfo) {
-            
+            result.Info = adInfo;
         }
 
         // Invoked when end user clicked on the interstitial ad
         override protected void InterstitialOnAdClickedEvent(IronSourceAdInfo adInfo) {
-            Debug.Log("unity-script: I got InterstitialOnAdClickedEvent With AdInfo " + adInfo.ToString());
+            result.Info = adInfo;
             result.UserClicked = true;
         }
 
@@ -57,19 +56,19 @@ namespace IPTech.UnityServices.Internal {
         // This callback is not supported by all networks, and we recommend using it only if  
         // it's supported by all networks you included in your build. 
         override protected void InterstitialOnAdShowSucceededEvent(IronSourceAdInfo adInfo) {
-                        
+            result.Info = adInfo;
         }
 
         // Invoked when the ad failed to show.
         override protected void InterstitialOnAdShowFailedEvent(IronSourceError ironSourceError, IronSourceAdInfo adInfo) {
-            Debug.Log("unity-script: I got InterstitialOnAdShowFailedEvent With Error " +ironSourceError.ToString()+ " And AdInfo " + adInfo.ToString());
+            result.Info = adInfo;
             result.AdResult = AdResult.FailedToShow;
             showState = ShowState.FinishedShowing;
         }
 
         // Invoked when the interstitial ad closed and the user went back to the application screen.
         override protected void InterstitialOnAdClosedEvent(IronSourceAdInfo adInfo) {
-            Debug.Log("unity-script: I got InterstitialOnAdClosedEvent With AdInfo " + adInfo.ToString());
+            result.Info = adInfo;
             result.AdResult = AdResult.Watched;
             showState = ShowState.FinishedShowing;
         }
