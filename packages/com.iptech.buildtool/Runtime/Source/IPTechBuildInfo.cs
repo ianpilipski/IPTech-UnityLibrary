9ea861012c06427e3f4bf96a2f757e0ad70a02cd
Ian Pilipski <ian@pilipski.com>
2024-12-20 20:00:00 -0500

    feat(buildtools): added ability to create buildinfo manually

--- packages/com.iptech.buildtool/Runtime/Source/IPTechBuildInfo.cs
+++ packages/com.iptech.buildtool/Runtime/Source/IPTechBuildInfo.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 
 namespace IPTech.BuildTool {
+    [CreateAssetMenu(menuName = "IPTech/BuildTool/IPTechBuildInfo", fileName = nameof(IPTechBuildInfo))]
     public class IPTechBuildInfo : ScriptableObject {
         public string BuildNumber;
 
